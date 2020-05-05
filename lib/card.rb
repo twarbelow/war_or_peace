9b1559aa65f0c8517ede13904d3a2d4636eb8274
“Taija” <taija.warbelow@google.com>
2020-05-04 20:44:28 -0700

    Create card.rb

---
+++ lib/card.rb
@@ -0,0 +1,16 @@
+class Card
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+  def suit
+    @suit
+  end
+  def value
+    @value
+  end
+  def rank
+    @rank
+  end
+end
