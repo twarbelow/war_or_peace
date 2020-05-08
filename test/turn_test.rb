7b7adb9ac0aab2fd4ecd8944d71a1ae97eaa62cd
“Taija” <taija.warbelow@google.com>
2020-05-07 20:35:21 -0700

    create turn class and turn_test

---
+++ test/turn_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def setup
+  end
+end
