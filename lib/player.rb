56ff1ec7fd842b1fe1f06dae698e21ee3aa8b927
“Taija” <taija.warbelow@google.com>
2020-05-07 19:53:34 -0700

    initialize Player, test_it_exists

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,10 @@
 class Player
+  attr_reader :name, :deck
 
-  def initialize
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
   end
+
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -1,3 +1,56 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/player'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+  end
+
+  def test_it_exists
+    player = Player.new('Clarisa', @deck)
+
+    assert_instance_of Player, player
+  end
+end
+
+
+
+# player = Player.new('Clarisa', deck)
+# #=> #<Player:0x007f9cc3b4c988 @deck=#<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>, @name="Clarisa">
+#
+# player.name
+# #=> "Clarisa"
+#
+# player.deck
+# #=> #<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>
+#
+# player.has_lost?
+# #=> false
+#
+# player.deck.remove_card
+# #=> #<Card:0x007f9cc3a73a98 @rank=12, @suit=:diamond, @value="Queen">
+#
+# player.has_lost?
+# #=> false
+#
+# player.deck.remove_card
+# #=> #<Card:0x007f9cc3a03720 @rank=3, @suit=:spade, @value="3">
+#
+# player.has_lost?
+# #=> false
+#
+# player.deck.remove_card
+# #=> #<Card:0x007f9cc3a44c98 @rank=14, @suit=:heart, @value="Ace">
+#
+# player.has_lost?
+# #=> true
+#
+# player.deck
+# #=> #<Deck:0x007f9cc396bdf8 @cards=[]>
