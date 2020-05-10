83ddb8293c995038d8633ef01d6e07437d2ce9dc
“Taija” <taija.warbelow@google.com>
2020-05-10 14:04:09 -0700

    Finish iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,13 +25,18 @@ class Turn
   def winner
     if self.type == :basic
       if player1.deck.cards[0].rank >= player2.deck.cards[0].rank
-        player1.name
+        @best = player1.name
       else
-        player2.name
+        @best = player2.name
       end
     elsif self.type == :war
-      #code here
+      if player1.deck.cards[2].rank >= player2.deck.cards[2].rank
+        @best = player1.name
+      else
+        @best = player2.name
+      end
     else self.type == :mutually_assured_destruction
+      "No Winner!"
     end
   end
 
@@ -40,9 +45,10 @@ class Turn
     if self.type == :basic
       spoils_of_war.concat [player1.deck.cards.shift, player2.deck.cards.shift]
     elsif self.type == :war
-      #code here
-    else
-      #code here
+      spoils_of_war.concat player1.deck.cards.shift(3), player2.deck.cards.shift(3)
+    else # type == mutually assured destruction
+      player1.deck.cards.shift(3)
+      player2.deck.cards.shift(3)
     end
   end
 
@@ -54,18 +60,3 @@ class Turn
     end
   end
 end
-
-# type: a turn is :basic, :war, or :mutually_assured_destruction.
-# A :basic turn is one in which the rank_of_card_at(0) from the players’ decks are not the same rank.
-# A :war turn occurs when both players’ rank_of_card_at(0) are the same.
-# :mutually_assured_destruction occurs when both players’ rank_of_card_at(0) AND rank_of_card_at(2) are the same.
-# winner: this method will determine the winner of the turn.
-# if the turn has a type of :basic, it will return whichever player has a higher rank_of_card_at(0)
-# if the turn has a type of :war the winner will be whichever player has a higher rank_of_card_at(2)
-# if the turn has a type of :mutually_assured_destruction the method will return No Winner.
-# pile_cards: when this method is called, cards will be sent from the players’ decks into the @spoils_of_war based on these rules
-# for a :basic turn, each player will send one card (the top card) to the spoils pile
-# for a :war turn, each player will send three cards (the top three cards) to the spoils pile
-# for a :mutually_assured_destruction turn, each player will remove three cards from play (the top three cards in their deck). These cards are not sent to the spoils pile, they are simply removed from each players’ deck.
-
-# award_spoils: this method will add each of the cards in the @spoils_of_war array to the winner of the turn.
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -36,7 +36,7 @@ class TurnTest < Minitest::Test
       assert_equal [], @turn.spoils_of_war
     end
 
-    def test_it_has_a_type
+    def test_it_has_correct_type
       assert_equal :basic, @turn.type
     end
 
@@ -79,86 +79,62 @@ class TurnTest < Minitest::Test
       @turn = Turn.new(@player1, @player2)
     end
 
-    def test_it_has_a_type
+    def test_it_has_correct_type
       assert_equal :war, @turn.type
     end
 
-    # def test_it_has_a_winner
-    #   assert_equal "Aurora", @turn.winner
-    # end
+    def test_it_has_a_winner
+      assert_equal "Aurora", @turn.winner
+    end
+
+    def test_it_has_spoils_of_war
+      @turn.pile_cards
+      assert_equal [@card1, @card2, @card5, @card4, @card3, @card6], @turn.spoils_of_war
+    end
+
+    def test_it_can_award_spoils
+      @turn.pile_cards
+      @turn.award_spoils
+
+      assert_equal [@card8], @player1.deck.cards
+      assert_equal [@card7 ,@card1, @card2, @card5, @card4, @card3, @card6], @player2.deck.cards
+    end
+  end
+
+  class MutuallyAssuredDestructionTest < TurnTest
+    def setup
+      @card1 = Card.new(:heart, 'Jack', 11)
+      @card2 = Card.new(:heart, '10', 10)
+      @card3 = Card.new(:heart, '9', 9)
+      @card4 = Card.new(:diamond, 'Jack', 11)
+      @card5 = Card.new(:heart, '8', 8)
+      @card6 = Card.new(:diamond, '8', 8)
+      @card7 = Card.new(:heart, '3', 3)
+      @card8 = Card.new(:diamond, '2', 2)
+
+      @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+      @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+
+      @player1 = Player.new("Megan", @deck1)
+      @player2 = Player.new("Aurora", @deck2)
+
+      @turn = Turn.new(@player1, @player2)
+    end
+
+    def test_it_has_correct_type
+      assert_equal :mutually_assured_destruction, @turn.type
+    end
+
+    def test_it_has_a_winner
+      assert_equal "No Winner!", @turn.winner
+    end
+
+    def test_it_has_spoils_of_war
+      @turn.pile_cards
+
+      assert_equal [], @turn.spoils_of_war
+      assert_equal [@card8], @player1.deck.cards
+      assert_equal [@card7], @player2.deck.cards
+    end
   end
 end
-
-
-
-# winner = turn.winner
-# #=> #<Player:0x007fc42aab6b70 @deck=#<Deck:0x007fc42a248678...>, @name="Aurora">
-#
-# turn.pile_cards
-#
-# turn.spoils_of_war
-# #=> [#<Card:0x007fc42a170fe8...>, #<Card:0x007fc42a0f1b58...>, #<Card:0x007fc42a2149e0...>, #<Card:0x007fc42a205508...>, #<Card:0x007fc42aa85a98...>, #<Card:0x007fc42a1e4790...>]
-#
-# turn.award_spoils(winner)
-#
-# player1.deck
-# #=> #<Deck:0x007fc42a05a258 @cards=[#<Card:0x007fc42a1580d8...>]>
-#
-# player2.deck
-# #=> #<Deck:0x007fc42a248678 @cards=[#<Card:0x007fc42a1b4c98...>, #<Card:0x007fc42a170fe8...>, #<Card:0x007fc42a0f1b58...>, #<Card:0x007fc42a2149e0...>, #<Card:0x007fc42a205508...>, #<Card:0x007fc42aa85a98...>, #<Card:0x007fc42a1e4790...>]>
-# # turn type :mutually_assured_destruction
-# require './lib/card'
-# #=> true
-# require './lib/deck'
-# #=> true
-# require './lib/player'
-# #=> true
-# require './lib/turn'
-# #=> true
-#
-# card1 = Card.new(:heart, 'Jack', 11)
-# #=> #<Card:0x007fadb0922f38 @rank=11, @suit=:heart, @value="Jack">
-# card2 = Card.new(:heart, '10', 10)
-# #=> #<Card:0x007fadaf8c8458 @rank=10, @suit=:heart, @value="10">
-# card3 = Card.new(:heart, '9', 9)
-# #=> #<Card:0x007fadaf9ab320 @rank=9, @suit=:heart, @value="9">
-# card4 = Card.new(:diamond, 'Jack', 11)
-# #=> #<Card:0x007fadb1046fa8 @rank=11, @suit=:diamond, @value="Jack">
-# card5 = Card.new(:heart, '8', 8)
-# #=> #<Card:0x007fadb0a247d8 @rank=8, @suit=:heart, @value="8">
-# card6 = Card.new(:diamond, '8', 8)
-# #=> #<Card:0x007fadaf9e8e28 @rank=8, @suit=:diamond, @value="8">
-# card7 = Card.new(:heart, '3', 3)
-# #=> #<Card:0x007fadaf130858 @rank=3, @suit=:heart, @value="3">
-# card8 = Card.new(:diamond, '2', 2)
-# #=> #<Card:0x007fadaf96b180 @rank=2, @suit=:diamond, @value="2">
-#
-# deck1 = Deck.new([card1, card2, card5, card8])
-# #=> #<Deck:0x007fadaf94aae8 @cards=[#<Card:0x007fadb0922f38...>, #<Card:0x007fadaf8c8458...>, #<Card:0x007fadb0a247d8...>, #<Card:0x007fadaf96b180...>]>
-#
-# deck2 = Deck.new([card4, card3, card6, card7])
-# #=> #<Deck:0x007fadb1086f40 @cards=[#<Card:0x007fadb1046fa8...>, #<Card:0x007fadaf9ab320...>, #<Card:0x007fadaf9e8e28...>, #<Card:0x007fadaf130858...>]>
-#
-# player1 = Player.new("Megan", deck1)
-# #=> #<Player:0x007fadaf8b16e0 @deck=##<Deck:0x007fadaf94aae8 ...>, @name="Megan">
-#
-# player2 = Player.new("Aurora", deck2)
-# #=> #<Player:0x007fadb0a47800 @deck=#<Deck:0x007fadb1086f40 ...>, @name="Aurora">
-#
-# turn = Turn.new(player1, player2)
-# #=> #<Turn:0x007fadaf240fe0 @player1=#<Player:0x007fadaf8b16e0 @deck=##<Deck:0x007fadaf94aae8 ...>, @name="Megan">, @player2= #<Player:0x007fadb0a47800 @deck=#<Deck:0x007fadb1086f40 ...>, @name="Aurora">, @spoils_of_war=[]>
-#
-# turn.type
-# #=> :mutually_assured_destruction
-#
-# winner = turn.winner
-# #=> "No Winner"
-#
-# turn.pile_cards
-#
-# turn.spoils_of_war
-# #=> []
-# player1.deck
-# #=> #<Deck:0x007fadaf94aae8 @cards=[#<Card:0x007fadaf96b180...>]>
-# player2.deck
-# #=> #<Deck:0x007fadb1086f40 @cards=[#<Card:0x007fadaf130858...>]>
