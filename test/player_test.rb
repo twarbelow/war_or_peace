e4c7e271dee8d5c5cfed6d0a788df783c1d007b4
“Taija” <taija.warbelow@google.com>
2020-05-10 14:54:54 -0700

    add player_test to iteration_2 branch

---
+++ test/player_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
+  def test_it_exists
+    assert_instance_of Player, @player
+  end
+
+  def test_it_has_name
+    assert_equal "Clarisa", @player.name
+  end
+
+  def test_it_has_deck
+    assert_equal @deck, @player.deck
+  end
+
+  def test_it_knows_loss
+    @player.deck.remove_card
+    @player.deck.remove_card
+    @player.deck.remove_card
+
+    assert_equal true, @player.has_lost?
+  end
+end
