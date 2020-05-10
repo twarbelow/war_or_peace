339eec7fc7f7392588330281de9a8be1ba3c0098
“Taija” <taija.warbelow@google.com>
2020-05-09 18:33:24 -0700

    Peripheral updates to card, card_test, player

--- lib/card.rb
+++ lib/card.rb
@@ -1,16 +1,8 @@
 class Card
+  attr_reader :suit, :value, :rank
   def initialize(suit, value, rank)
     @suit = suit
     @value = value
     @rank = rank
-  end
-  def suit
-    @suit
-  end
-  def value
-    @value
-  end
-  def rank
-    @rank
   end
 end
