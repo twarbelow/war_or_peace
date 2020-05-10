747486528aee363ab1740dd06621cb4a8c49e686
“Taija” <taija.warbelow@google.com>
2020-05-10 15:25:03 -0700

    create runner for war_or_peace

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,60 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+2, Heart, 2
+3, Heart, 3
+4, Heart, 4
+5, Heart, 5
+6, Heart, 6
+7, Heart, 7
+8, Heart, 8
+9, Heart, 9
+10, Heart, 10
+Jack, Heart, 11
+Queen, Heart, 12
+King, Heart, 13
+Ace, Heart, 14
+
+2, Diamond, 2
+3, Diamond, 3
+4, Diamond, 4
+5, Diamond, 5
+6, Diamond, 6
+7, Diamond, 7
+8, Diamond, 8
+9, Diamond, 9
+10, Diamond, 10
+Jack, Diamond, 11
+Queen, Diamond, 12
+King, Diamond, 13
+Ace, Diamond, 14
+
+2, Spade, 2
+3, Spade, 3
+4, Spade, 4
+5, Spade, 5
+6, Spade, 6
+7, Spade, 7
+8, Spade, 8
+9, Spade, 9
+10, Spade, 10
+Jack, Spade, 11
+Queen, Spade, 12
+King, Spade, 13
+Ace, Spade, 14
+
+2, Club, 2
+3, Club, 3
+4, Club, 4
+5, Club, 5
+6, Club, 6
+7, Club, 7
+8, Club, 8
+9, Club, 9
+10, Club, 10
+Jack, Club, 11
+Queen, Club, 12
+King, Club, 13
+Ace, Club, 14
